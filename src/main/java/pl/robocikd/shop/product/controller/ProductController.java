d4ea43edbbbf98284c03df6535f6c716e14a2bda
Damian <robocikd@wp.pl>
2022-11-19 16:12:59 +0100

    products paging

--- src/main/java/pl/robocikd/shop/product/controller/ProductController.java
+++ src/main/java/pl/robocikd/shop/product/controller/ProductController.java
@@ -1,12 +1,13 @@
 package pl.robocikd.shop.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.robocikd.shop.product.model.Product;
 import pl.robocikd.shop.product.service.ProductService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +16,7 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getProducts();
+    public Page<Product> getProducts(@PageableDefault(size = 10) Pageable pageable) {
+        return productService.getProducts(pageable);
     }
 }
--- src/main/java/pl/robocikd/shop/product/service/ProductService.java
+++ src/main/java/pl/robocikd/shop/product/service/ProductService.java
@@ -1,11 +1,11 @@
 package pl.robocikd.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.robocikd.shop.product.model.Product;
 import pl.robocikd.shop.product.repositiry.ProductRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +13,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
