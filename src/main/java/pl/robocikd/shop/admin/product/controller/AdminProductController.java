4ec2c118ad5e9bc1add5bb8e6fa891ae346c8f6b
Damian <robocikd@wp.pl>
2022-11-22 22:28:20 +0100

    delete product
    add feature to delete product

--- src/main/java/pl/robocikd/shop/admin/product/controller/AdminProductController.java
+++ src/main/java/pl/robocikd/shop/admin/product/controller/AdminProductController.java
@@ -3,6 +3,7 @@ package pl.robocikd.shop.admin.product.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,11 @@ public class AdminProductController {
     public AdminProduct updateProduct(@RequestBody @Valid AdminProductDto adminProductDto, @PathVariable Long id) {
         return adminProductService.updateProduct(mapAdminProduct(adminProductDto, id)
         );
+    }
+
+    @DeleteMapping("/admin/product/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        adminProductService.deleteProduct(id);
     }
 
     private static AdminProduct mapAdminProduct(AdminProductDto adminProductDto, Long id) {
--- src/main/java/pl/robocikd/shop/admin/product/service/AdminProductService.java
+++ src/main/java/pl/robocikd/shop/admin/product/service/AdminProductService.java
@@ -27,4 +27,8 @@ public class AdminProductService {
     public AdminProduct updateProduct(AdminProduct adminProduct) {
         return adminProductRepository.save(adminProduct);
     }
+
+    public void deleteProduct(Long id) {
+        adminProductRepository.deleteById(id);
+    }
 }
