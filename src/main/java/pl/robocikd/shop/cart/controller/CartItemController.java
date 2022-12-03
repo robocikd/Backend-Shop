2928a756a70133cf913d88a4c9955f04b6f4eb04
Damian <robocikd@wp.pl>
2022-12-03 17:29:03 +0100

    cart icon product counter

--- src/main/java/pl/robocikd/shop/cart/controller/CartItemController.java
+++ src/main/java/pl/robocikd/shop/cart/controller/CartItemController.java
@@ -2,6 +2,7 @@ package pl.robocikd.shop.cart.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,11 @@ public class CartItemController {
     @DeleteMapping("/{id}")
     public void deleteCartItem(@PathVariable Long id) {
         cartItemService.delete(id);
+    }
+
+    @GetMapping("/count/{cartId}")
+    public Long countItemInCart(@PathVariable Long cartId) {
+        return cartItemService.countItemInCart(cartId);
     }
 
 }
--- src/main/java/pl/robocikd/shop/cart/respository/CartItemRepository.java
+++ src/main/java/pl/robocikd/shop/cart/respository/CartItemRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.robocikd.shop.cart.model.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Long countByCartId(Long cartId);
 }
