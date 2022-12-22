66d559b4fb78d660b25465f391aa06b03e9045fc
Damian <robocikd@wp.pl>
2022-12-22 16:42:39 +0100

    secure get orders endpoint

--- src/main/java/pl/robocikd/shop/security/SecurityConfig.java
+++ src/main/java/pl/robocikd/shop/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.robocikd.shop.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfig {
     ) throws Exception {
         http.authorizeRequests(authorize -> authorize
                 .antMatchers("/admin/**").hasAnyRole(UserRole.ROLE_ADMIN.getRole())
+                .antMatchers(HttpMethod.GET, "/orders").authenticated()
                 .anyRequest().permitAll());
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
